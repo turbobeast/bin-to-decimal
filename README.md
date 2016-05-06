c21b8b557d1d8f4bd9e94c28dfc8ae36ca1678b5
turbobeast <georgemichaelfaust@gmail.com>
2016-05-05 22:40:52 -0400

    convert to string, throw on invalid input - no octal literals in readme

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # bin-to-decimal
-converts binary numbers into decimal (base 10) 
+converts binary numbers into decimal (base 10)
 
 Don't get excited about this. This was an exercise. You should just do.
 
 ```
-let bin = 01010101;
+let bin = 1010101;
 let dec = parseInt((bin).toString(), 2);
 ```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const binToDecimal = bin => {
-  "use strict";
+  'use strict';
+  if (typeof bin !== 'string') {
+    bin = String(bin);
+  }
   const l = bin.length;
   let dec = 0;
   for(let i = l-1; i >= 0; --i) {
+    if(String(bin[i]) !== '1' && String(bin[i]) !== '0') {
+      throw new Error(`binToDecimal can only convert binary numbers. ${bin} is not valid`);
+    }
     let one_zer = parseInt(bin[i], 2);
     dec += one_zer * Math.pow(2, l-i-1);
   }
